a413079b2070ae9a7c44e81a2ea01091b2db1b29
Roman Mazeev <roman.mazeev@lightspeedhq.com>
2023-01-16 02:21:48 +0100

    Small update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -14,7 +14,7 @@ let package = Package(
             name: "MRZParser",
             dependencies: []),
         .testTarget(
-            name: "MRZParserTests",
+            name: "MRZParserIntegrationTests",
             dependencies: ["MRZParser"]),
     ]
 )
