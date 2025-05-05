a7dd3a8c28fe4f829308ffbae5a25bac4674da82
Roman Mazeev <me@romanmazeev.com>
2025-05-05 15:51:24 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["MRZParser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.8.0"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.2"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.3")
     ],
     targets: [
